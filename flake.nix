f095d12dbc4e3179eaa86e6e21d73bbe3391b8b1
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-01-21 19:03:40 +0100

    flake

--- flake.nix
+++ flake.nix
@@ -1,37 +1,88 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    configuration.url = "github:ivandimitrov8080/configuration.nix";
+    systems.url = "github:nix-systems/default";
+    # nvim config helper
+    nixvim-flake.url = "github:nix-community/nixvim";
+    nixvim-flake.inputs.nixpkgs.follows = "nixpkgs";
+    # neovim latest version
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
-
   outputs =
-    { nixpkgs, ... }:
+    inputs@{
+      self,
+      nixpkgs,
+      configuration,
+      systems,
+      nixvim-flake,
+      neovim-nightly-overlay,
+      devenv,
+    }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      pname = "cv";
-      version = "0.1.1";
-      src = ./.;
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      packages.${system}.default = pkgs.mkYarnPackage {
-        inherit pname version src;
-        nativeBuildInputs = [ pkgs.bun ];
-        offlineCache = pkgs.fetchYarnDeps {
-          yarnLock = "${src}/yarn.lock";
-          hash = "sha256-HKoIv3YMKy5uZ/wT2grg+Z6k4CMykY9dj/okfA2uias=";
-        };
-        buildPhase = ''
-          yarn --offline build
-        '';
-        doDist = false;
-        postInstall = ''
-          ls -alh
-          rm -rf $out/lib
-          rm -rf $out/libexec
-          rm -rf $out/bin
-        '';
-      };
+      packages = eachSystem (system: {
+        hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        default = self.packages.x86_64-linux.hello;
+      });
+      devShells = eachSystem (
+        system:
+        let
+          nixvim-default = nixvim-flake.legacyPackages.${system}.makeNixvim {
+            package = neovim-nightly-overlay.packages.${system}.default;
+          };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                nixvim = nixvim-default;
+              })
+              configuration.overlays.default
+            ];
+          };
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                packages = with pkgs; [ nixvim.haskell ];
+              }
+            ];
+          };
+        }
+      );
+      checks = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ configuration.overlays.default ];
+          };
+        in
+        {
+          default = pkgs.testers.runNixOSTest {
+            name = "test";
+            nodes = {
+              machine =
+                { pkgs, ... }:
+                {
+                  environment.systemPackages = [ pkgs.hello ];
+                };
+            };
+            testScript =
+              #py
+              ''
+                machine.wait_for_unit("multi-user.target");
+                machine.succeed("hello");
+              '';
+          };
+        }
+      );
     };
 }
