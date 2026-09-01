0b3a4b15ccd44611798c79e946cf710313458a4e
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-09-01 20:55:13 +0200

    cleanup of nixvim

--- flake.nix
+++ flake.nix
@@ -1,14 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    configuration.url = "github:ivandimitrov8080/configuration.nix";
     systems.url = "github:nix-systems/default";
-    # nvim config helper
-    nixvim-flake.url = "github:nix-community/nixvim";
-    nixvim-flake.inputs.nixpkgs.follows = "nixpkgs";
-    # neovim latest version
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
@@ -16,10 +9,7 @@
   outputs =
     inputs@{
       nixpkgs,
-      configuration,
       systems,
-      nixvim-flake,
-      neovim-nightly-overlay,
       devenv,
       treefmt-nix,
       ...
@@ -60,17 +50,8 @@
       devShells = eachSystem (
         system:
         let
-          nixvim-default = nixvim-flake.legacyPackages.${system}.makeNixvim {
-            package = neovim-nightly-overlay.packages.${system}.default;
-          };
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [
-              (_final: _prev: {
-                nixvim = nixvim-default;
-              })
-              configuration.overlays.default
-            ];
           };
         in
         {
@@ -78,11 +59,8 @@
             inherit inputs pkgs;
             modules = [
               {
+                devenv.root = "/home/ivand/src/cv";
                 packages = with pkgs; [
-                  (nixvim.main.extend {
-                    lsp.servers.jsonls.enable = true;
-                    lsp.servers.tinymist.enable = true;
-                  })
                   typst
                 ];
                 git-hooks.hooks = {
@@ -118,33 +96,6 @@
             statix.enable = true;
           };
         }).config.build.wrapper
-      );
-      checks = eachSystem (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ configuration.overlays.default ];
-          };
-        in
-        {
-          default = pkgs.testers.runNixOSTest {
-            name = "test";
-            nodes = {
-              machine =
-                { pkgs, ... }:
-                {
-                  environment.systemPackages = [ pkgs.hello ];
-                };
-            };
-            testScript =
-              #py
-              ''
-                machine.wait_for_unit("multi-user.target");
-                machine.succeed("hello");
-              '';
-          };
-        }
       );
     };
 }
